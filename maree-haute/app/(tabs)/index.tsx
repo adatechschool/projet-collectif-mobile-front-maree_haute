ce3f0cd9ac0406e39e2a0c129fe215a50f66f793
realalexis <alexismoulliere44@gmail.com>
2024-04-30 15:52:29 +0200

    add list airtable

--- maree-haute/app/(tabs)/index.tsx
+++ maree-haute/app/(tabs)/index.tsx
@@ -1,13 +1,39 @@
 import { StyleSheet, Text, View } from "react-native";
-import { Link } from "expo-router";
+import { Stack, Tabs } from "expo-router";
+import React, { useState, useEffect } from "react";
 
 export default function Page() {
+  const [data, setData] = useState([]);
+  const fetchData = async () => {
+    const response = await fetch(
+      "https://api.airtable.com/v0/appfYZhtggrzhPbaz/Surf%20Destinations?&view=By%20Surf%20Break",
+      {
+        method: "GET",
+        headers: {
+          Authorization:
+            "Bearer patI2bxmroBSEpdLF.80f18186cf70c1bd9380a09de6dee32300da1b6a3a241c775f555f91c16facb6",
+        },
+      }
+    );
+    const data = await response.json();
+    const records = data.records.map((record) => record.fields);
+    console.log(records);
+    setData(records);
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
     <View style={styles.container}>
       <View style={styles.main}>
         <Text style={styles.title}>Hello World</Text>
+        {data.map((record) => (
+          <Text key={record.Destination}>{record.Destination}</Text>
+        ))}
+        <Text style={styles.subtitle}>Welcome to your new app.</Text>
         <Text style={styles.subtitle}>This is the first page of your app.</Text>
-        <Link href="/TestPage">Test page</Link>
       </View>
     </View>
   );
